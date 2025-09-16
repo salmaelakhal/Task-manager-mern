9fb12bbe031a0cf2fa16c32f88f689d653d2bbed
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-09-16 21:37:34 +0100

    Add user management routes and controllers (get users with task counts, get user by ID)

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,1 +1,15 @@
-  
+const express = require("express");
+const { protect, adminOnly } = require("../middlewares/authMiddleware");
+const {
+  getUsers,
+  getUserById,
+  deleteUser,
+} = require("../controllers/userController");
+
+const router = express.Router();
+
+// User Managent Routes
+router.get("/", protect, adminOnly, getUsers); // get all users (admin only)
+router.get("/:id", protect, getUserById); //get a specific user
+
+module.exports = router;
