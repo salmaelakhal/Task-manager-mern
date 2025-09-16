4a99e90065102776788dcb06de5aba80fa732739
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-09-16 15:36:49 +0100

    feat: add User & Task mongoose models and setup Express server with MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,11 @@
 require ("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
-
+const connectDB = require("./config/db");
 
 const app = express();
-
+ 
 
 // Middleware to handlle CORS
 app.use(
@@ -18,8 +16,19 @@ app.use(
     })
 );
 
+
+
+//connect to MongoDB
+connectDB();
+
+
+
+
 // Middleware
 app.use(express.json());
+
+
+
 
 
 // Routes
