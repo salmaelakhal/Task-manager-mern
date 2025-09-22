19defcff01d9edc976fd0dcd1cbf019c1711c71c
Salma Elakhal <salma.elakhal.pro@gmail.com>
2025-09-22 17:52:48 +0100

    API Call to Fetch Dashboard Data

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,7 +1,10 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import { UserContext } from "../../context/userContext";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const Dashboard = () => {
   useUserAuth();
@@ -14,7 +17,29 @@ const Dashboard = () => {
   const [pieChartData, setPieChartData] = useState(null);
   const [barChartData, setBarChartData] = useState(null);
 
-  return <DashboardLayout activeMenu="Dashboard">dashboard</DashboardLayout>;
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA
+      );
+    } catch (error) {
+      console.error("Error fetching users: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getDashboardData();
+
+    return () => {};
+
+  }, []);
+
+
+  return <DashboardLayout activeMenu="Dashboard">
+    <div className="">
+
+    </div>
+    </DashboardLayout>;
 };
 
 export default Dashboard;
